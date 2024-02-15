ed8bf8b4be9aa217ade1eeedbd2435a362810da2
Kapil Garg <gargkapil90@gmail.com>
2024-02-15 18:48:11 +0530

    adding thread concepts

--- src/main/java/ConcurrencyAndParallelism/PrintNumbers/Client.java
+++ src/main/java/ConcurrencyAndParallelism/PrintNumbers/Client.java
@@ -1,11 +1,19 @@
 package ConcurrencyAndParallelism.PrintNumbers;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Client {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++){
+//        ExecutorService es = Executors.newCachedThreadPool();
+        ExecutorService es = Executors.newFixedThreadPool(10);
+
+        for (int i = 1; i <= 500000; i++){
             PrintNumber pn = new PrintNumber(i);
-            Thread th = new Thread(pn);
-            th.start();
+//            Thread th = new Thread(pn);
+//            th.start();
+            es.submit(pn);
+
         }
     }
 }
